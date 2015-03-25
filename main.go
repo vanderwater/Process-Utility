8a7d100d8abc2a3a35128579d917669d96f2d45a
vndrwtr2 <vndrwtr2@gmail.com>
2015-03-25 15:15:49 -0500

    protofiles made, no support yet

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import(
 	"os/exec"
 	"strconv"
 	"strings"
+	"process.proto"
+	"github.com/golang/protobuff/proto"
 )
 
 type process struct {
@@ -21,23 +23,23 @@ func main(){
 	ps := exec.Command("ps", "-e", "-o pid,vsz,time,comm") 
 	// todo: error checking
 	output, _ := ps.Output()
-	process_map := make(map[int]process);
-
+	processMap := make(map[int]process);
+	
 	// Loop over all the outputs
 	// todo: think of a better name for s, error checking for atoi
-	var current_process process
+	var currentProcess process
 	for i, s := range strings.Split(string(output), "\n") {
 		if i == 0 { continue }  // skip line 1 if unskipped, gets headers
 		if len(s) == 0 { continue } // skip last line, if unskipped causes index oob error
 		ps_line := strings.Fields(s)
 		pid, _ := strconv.Atoi(ps_line[0])
-		current_process.pid = pid
-		current_process.vsz, _ = strconv.Atoi(ps_line[1])
-		current_process.uptime = ps_line[2]
-		current_process.comm = ps_line[3]
-		process_map[pid] = current_process
+		currentProcess.pid = pid
+		currentProcess.vsz, _ = strconv.Atoi(ps_line[1])
+		currentProcess.uptime = ps_line[2]
+		currentProcess.comm = ps_line[3]
+		processMap[pid] = currentProcess
 	}
-	for k := range process_map {
-		fmt.Printf("Process: %d  VSZ:%d  Time: %s Comm: %s \n", process_map[k].pid,  process_map[k].vsz, process_map[k].uptime, process_map[k].comm)
-		}
+	for k := range processMap {
+		fmt.Printf("Process: %d  VSZ:%d  Time: %s Comm: %s \n", processMap[k].pid,  processMap[k].vsz, processMap[k].uptime, processMap[k].comm)
+	}	
 }
