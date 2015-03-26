bb7746ab6778864e5aabe7e7f7a8523f5941cb20
vndrwtr2 <vndrwtr2@gmail.com>
2015-03-26 01:11:07 -0500

    First working commit

--- main.go
+++ main.go
@@ -3,28 +3,23 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/exec"
 	"time"
-	"io"
 //	"flag"		todo: implement -h
 	"strconv"
 	"strings"
-	"github.com/golang/protobuff/proto"
+	"github.com/golang/protobuf/proto"
+	"processUtility"
 )
 
-type process struct {
-	pid, vsz int
-	uptime, comm string	
-}
 
 // This currently organizes all relevant information into a map
 
 // todo: instead of putting into map, put directly into protobuf thing. Need to research protobuf more
 
-func GetProcessInfo(){
+func GetProcessInfo() ([]byte, error){
 	
 	processSet := new(processUtility.ProcessSet)	
-	
-	currentProcess := new(processUtility.Process)
 	
 	// Call ps for process info, -e gives all, -o gives specified output
 	ps := exec.Command("ps", "-e", "-o pid,vsz,time,comm") 
@@ -36,14 +31,18 @@ func GetProcessInfo(){
 	for i, s := range strings.Split(string(output), "\n") {
 		if i == 0 { continue }  // skip line 1 if unskipped, gets headers
 		if len(s) == 0 { continue } // skip last line, if unskipped causes index oob error
+		currentProcess := new(processUtility.Process)
 		ps_line := strings.Fields(s)
-		currentProcess.pid, _ := strconv.Atoi(ps_line[0])
-		currentProcess.vsz, _ = strconv.Atoi(ps_line[1])
-		currentProcess.uptime = ps_line[2]
-		currentProcess.comm = ps_line[3]
-		processSet.processes = append(processSet.processes, currentProcess)
+		//fmt.Println(ps_line)  For testing
+		pid, _ := strconv.Atoi(ps_line[0])
+		currentProcess.Pid = proto.Int32(int32(pid))
+		vsz, _ := strconv.Atoi(ps_line[1])
+		currentProcess.Vsz = proto.Int32(int32(vsz))
+		currentProcess.Time = &ps_line[2]
+		currentProcess.Comm = &ps_line[3]
+		processSet.Processes = append(processSet.Processes, currentProcess)
 	}
-
+	//fmt.Println(processSet) For testing
 	return proto.Marshal(processSet)
 }
 
@@ -54,19 +53,24 @@ func main(){
 		return
 	}
 	
-	waitTime := os.Args[1]
+	waitTime, _ := strconv.Atoi(os.Args[1])
 	
-	clock := time.Tick(waitTime * time.Second)
+	clock := time.NewTicker(time.Duration(waitTime) * time.Second)
 	
-	fileo, _ := os.Open("output.txt")	// todo: implement err checking
+	fileo, _ := os.Create("output.txt")	// todo: implement err checking
 	
-    defer func() {
-	    if err := fileo.Close(); err != nil {
-			 panic(err)
+	defer fileo.Close()
+	
+	go func() {
+		for now := range clock.C {
+			processInfo, _ := GetProcessInfo()
+			fileo.Write(processInfo)
+			fmt.Println("Logged at", now)
 		}
-    }()
+	}()
+	// Currently just runs 2 iterations
+	time.Sleep(time.Second * 11)
+	clock.Stop()
+	fmt.Println("Done")
 
-	for now := range clock {
-		fmt.Fprint(fileo, processUtility.GetProcessInfo())
-	}
 }
